a4b1df05d5c22b3e054c35adc3414f27a91dbe12
IamMOTY <moty1812200@Gmail.com>
2020-05-11 00:08:26 +0300

    Final version

--- src/TemplateCombination.cpp
+++ src/TemplateCombination.cpp
@@ -6,6 +6,132 @@
 #include "TemplateCombination.h"
 
 
+namespace {
+    bool check_same_values_expr(const TemplateCombination &templateCombination, const std::vector<Component> &fixed,
+                                const std::vector<int> &order) {
+        bool flag = true;
+        std::tm def{};
+        std::unordered_map<Expiration, std::tm> check;
+        for (int k = 0; k < templateCombination.count && flag; k++) {
+            Expiration temp_exp = templateCombination.legs[k].expiration;
+            if (temp_exp.ischeckable()) {
+                if (Time::comp(check[temp_exp], def) == 0) {
+                    check[temp_exp] = fixed[order[k]].expiration;
+                } else {
+                    if (Time::comp(check[temp_exp], fixed[order[k]].expiration) != 0) {
+                        flag = false;
+                        continue;
+                    }
+                }
+            }
+        }
+        return flag;
+    }
+
+
+    bool check_sequence_expr(const TemplateCombination &templateCombination, const std::vector<Component> &fixed,
+                             const std::vector<int> &order) {
+        bool flag;
+        std::tm saved{};
+        for (int k = 0; k < templateCombination.count && flag; k++) {
+            Expiration temp_exp = templateCombination.legs[k].expiration;
+            if (temp_exp.isbear()) {
+                saved = fixed[order[k]].expiration;
+            } else {
+                if (temp_exp.sym == '+') {
+                    Expiration prev_exp = templateCombination.legs[k - 1].expiration;
+                    if (prev_exp < temp_exp &&
+                        Time::comp(fixed[order[k - 1]].expiration, fixed[order[k]].expiration) <= 0) {
+                        flag = false;
+                        continue;
+                    }
+                }
+                if (temp_exp.isdelta()) {
+                    if (Time::comp(saved, fixed[order[k]].expiration) == -1) {
+                        flag = false;
+                        continue;
+                    }
+                    int test;
+                    switch (temp_exp.sym) {
+                        case 'q':
+                            test = Time::diffInQuart(saved, fixed[order[k]].expiration);
+                            break;
+                        case 'm':
+                            test = Time::diffInMonth(saved, fixed[order[k]].expiration);
+                            break;
+                        case 'd':
+                            test = Time::diffInDays(saved, fixed[order[k]].expiration);
+                            break;
+                        case 'y':
+                            test = Time::diffInYears(saved, fixed[order[k]].expiration);
+                    }
+                    if (temp_exp.count != test) {
+                        flag = false;
+                        continue;
+                    }
+                }
+            }
+        }
+        return flag;
+    }
+
+    bool check_sequence_strike(const TemplateCombination &templateCombination, const std::vector<Component> &fixed,
+                               const std::vector<int> &order) {
+        bool flag;
+        std::unordered_map<Strike, double> check;
+        for (int k = 0; k < templateCombination.count && flag; k++) {
+            if (Component::testOptions(fixed[order[k]].type)) {
+                Strike temp_strike = templateCombination.legs[k].strike;
+                if (temp_strike.ischeckable()) {
+                    if (check[temp_strike] == 0) {
+                        check[temp_strike] = fixed[order[k]].strike;
+                    } else {
+                        if (std::abs(check[temp_strike] - fixed[order[k]].strike) > 1e-7) {
+                            flag = false;
+                            continue;
+                        }
+                    }
+                }
+            }
+        }
+        return flag;
+    }
+
+
+    bool check_same_values_strike(const TemplateCombination &templateCombination, const std::vector<Component> &fixed,
+                                  const std::vector<int> &order) {
+        bool flag;
+        double saved;
+        for (int k = 0; k < templateCombination.count && flag; k++) {
+            if (Component::testOptions(fixed[order[k]].type)) {
+                Strike temp_strike = templateCombination.legs[k].strike;
+                if (temp_strike.isbear()) {
+                    saved = fixed[order[k]].strike;
+                } else {
+                    if (temp_strike.sym == '+') {
+                        Strike prev_strike = templateCombination.legs[k - 1].strike;
+                        if (prev_strike < temp_strike &&
+                            fixed[order[k]].strike <= fixed[order[k - 1]].strike) {
+                            flag = false;
+                            continue;
+                        }
+                    }
+                    if (temp_strike.sym == '-') {
+                        Strike prev_strike = templateCombination.legs[k - 1].strike;
+                        if (prev_strike < temp_strike &&
+                            fixed[order[k]].strike >= fixed[order[k - 1]].strike) {
+                            flag = false;
+                            continue;
+                        }
+                    }
+                }
+            }
+        }
+        return flag;
+    }
+
+} // namespace
+
 static std::map<std::string, CardinalityType> const string_to_cardinal = {{"fixed",    CardinalityType::FIXED},
                                                                           {"multiple", CardinalityType::MULTIPLE},
                                                                           {"more",     CardinalityType::MORE}};
@@ -41,8 +167,6 @@ TemplateCombination::match(const TemplateCombination &templateCombination, const
     }
 
 
-
-
     std::vector<Component> fixed = combination;
     if (templateCombination.cardinalityType == CardinalityType::MULTIPLE) {
         std::unordered_set<Component> temp(fixed.begin(), fixed.end());
@@ -52,14 +176,8 @@ TemplateCombination::match(const TemplateCombination &templateCombination, const
         }
     }
     order.clear();
-    for(int k = 0; k < templateCombination.count; k++) {
+    for (int k = 0; k < templateCombination.count; k++) {
         order.emplace_back(k);
-    }
-
-    if (templateCombination.name == "Inter commodity spread") {
-
-        1 + 2;
-
     }
 
     int end = 1;
@@ -71,119 +189,26 @@ TemplateCombination::match(const TemplateCombination &templateCombination, const
         }
 
         if (flag) {
-            std::tm def{};
-            std::unordered_map<Expiration, std::tm> check;
-            for (int k = 0; k < templateCombination.count && flag; k++) {
-                Expiration temp_exp = templateCombination.legs[k].expiration;
-                if (temp_exp.ischeckable()) {
-                    if (Time::comp(check[temp_exp], def) == 0) {
-                        check[temp_exp] = fixed[order[k]].expiration;
-                    } else {
-                        if (Time::comp(check[temp_exp], fixed[order[k]].expiration) != 0) {
-                            flag = false;
-                            continue;
-                        }
-                    }
-                }
-            }
+            flag = check_same_values_expr(templateCombination, fixed, order);
         }
 
         if (flag) {
-            std::tm saved{};
-            for (int k = 0; k < templateCombination.count && flag; k++) {
-                Expiration temp_exp = templateCombination.legs[k].expiration;
-                if (temp_exp.isbear()) {
-                    saved = fixed[order[k]].expiration;
-                } else {
-                    if (temp_exp.sym == '+') {
-                        Expiration prev_exp = templateCombination.legs[k - 1].expiration;
-                        if (prev_exp < temp_exp &&
-                            Time::comp(fixed[order[k - 1]].expiration, fixed[order[k]].expiration) <= 0) {
-                            flag = false;
-                            continue;
-                        }
-                    }
-                    if (temp_exp.isdelta()) {
-                    	if (Time::comp(saved, fixed[order[k]].expiration) == -1) {
-                            flag = false;
-                            continue;
-                        }
-                        int test;
-                        switch (temp_exp.sym) {
-                            case 'q':
-                                test = Time::diffInQuart(saved, fixed[order[k]].expiration);
-                                break;
-                            case 'm':
-                                test = Time::diffInMonth(saved, fixed[order[k]].expiration);
-                                break;
-                            case 'd':
-                                test = Time::diffInDays(saved, fixed[order[k]].expiration);
-                                break;
-                            case 'y':
-                                test = Time::diffInYears(saved, fixed[order[k]].expiration);
-                        }
-                        if (temp_exp.count != test) {
-                            flag = false;
-                            continue;
-                        }
-                    }
-                }
-            }
+            flag = check_sequence_expr(templateCombination, fixed, order);
         }
 
 
         if (flag) {
-            std::unordered_map<Strike, double> check;
-            for (int k = 0; k < templateCombination.count && flag; k++) {
-                if (Component::testOptions(fixed[order[k]].type)) {
-                    Strike temp_strike = templateCombination.legs[k].strike;
-                    if (temp_strike.ischeckable()) {
-                        if (check[temp_strike] == 0) {
-                            check[temp_strike] = fixed[order[k]].strike;
-                        } else {
-                            if (std::abs(check[temp_strike] - fixed[order[k]].strike) > 1e-7) {
-                                flag = false;
-                                continue;
-                            }
-                        }
-                    }
-                }
-            }
+            flag = check_same_values_strike(templateCombination, fixed, order);
         }
 
         if (flag) {
-            double saved;
-            for (int k = 0; k < templateCombination.count && flag; k++) {
-                if (Component::testOptions(fixed[order[k]].type)) {
-                    Strike temp_strike = templateCombination.legs[k].strike;
-                    if (temp_strike.isbear()) {
-                        saved = fixed[order[k]].strike;
-                    } else {
-                        if (temp_strike.sym == '+') {
-                            Strike prev_strike = templateCombination.legs[k - 1].strike;
-                            if (prev_strike < temp_strike &&
-                                fixed[order[k]].strike <= fixed[order[k - 1]].strike) {
-                                flag = false;
-                                continue;
-                            }
-                        }
-                        if (temp_strike.sym == '-') {
-                            Strike prev_strike = templateCombination.legs[k - 1].strike;
-                            if (prev_strike < temp_strike &&
-                                fixed[order[k]].strike >= fixed[order[k - 1]].strike) {
-                                flag = false;
-                                continue;
-                            }
-                        }
-                    }
-                }
-            }
+            flag = check_sequence_strike(templateCombination, fixed, order);
         }
 
         if (flag) {
             std::vector<int> temp = std::move(order);
             order.clear();
-            std::vector<int> count(fixed.size(),0);
+            std::vector<int> count(fixed.size(), 0);
             for (auto c: combination) {
                 for (int k = 0; k < templateCombination.count; k++) {
                     if (c == fixed[temp[k]]) {
@@ -223,10 +248,10 @@ int TemplateCombination::parse(pugi::xml_node node, TemplateCombination &templat
     return templateCombination.count;
 }
 
-TemplateCombination::TemplateCombination(std::string && name, CardinalityType cardinalityType, size_t count,
-                                         std::vector<TemplateComponent> && legs) : name(std::move(name)),
-                                                                                   cardinalityType(cardinalityType),
-                                                                                   count(count), legs(std::move(legs)) {}
+TemplateCombination::TemplateCombination(std::string &&name, CardinalityType cardinalityType, size_t count,
+                                         std::vector<TemplateComponent> &&legs) : name(std::move(name)),
+                                                                                  cardinalityType(cardinalityType),
+                                                                                  count(count), legs(std::move(legs)) {}
 
 TemplateCombination::TemplateCombination() {
     this->count = -1;
